35e142f6fb788b148832d3ecae7c90e5517570b6
hoytk6 <hoytk@knights.ucf.edu>
2020-02-11 20:03:31 -0500

    update planner.js

--- planner.js
+++ planner.js
@@ -1,1 +1,14 @@
+'use strict';
+
+const e = React.createElement;
+const renderButton = function(id, location, name) {
+	return (
+		e (
+			Button,
+			{toID: id, toWhere: location, toName: name},
+			null
+		)
+	);
+}
+
 
