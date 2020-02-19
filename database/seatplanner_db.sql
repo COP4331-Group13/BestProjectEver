530c7df30f3562d0479236d531aeb9f2cd38d0d5
Bruno Ebihara <49998058+brunoebihara@users.noreply.github.com>
2020-02-19 11:15:43 -0500

    added db dump, current table structures

---
+++ database/seatplanner_db.sql
@@ -0,0 +1,201 @@
+-- MySQL dump 10.16  Distrib 10.1.44-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: seatplanner
+-- ------------------------------------------------------
+-- Server version	10.1.44-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `event_pin` int(11) NOT NULL,
+  `event_time` date NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `max_people` int(11) NOT NULL,
+  `planner_id` int(11) NOT NULL,
+  `layout_id` int(11) NOT NULL,
+  `date_added` date NOT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `planner_id` (`planner_id`),
+  KEY `idx_event_pin` (`event_pin`),
+  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`planner_id`) REFERENCES `planner` (`planner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `guest`
+--
+
+DROP TABLE IF EXISTS `guest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `guest` (
+  `guest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `event_pin` int(11) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone_number` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `planner_overrides` varchar(100) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `confirmed` varchar(5) NOT NULL,
+  `plus_one` varchar(5) NOT NULL,
+  `date_added` datetime NOT NULL,
+  PRIMARY KEY (`guest_id`),
+  KEY `group_id` (`group_id`),
+  KEY `event_pin` (`event_pin`),
+  CONSTRAINT `guest_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`),
+  CONSTRAINT `guest_ibfk_2` FOREIGN KEY (`event_pin`) REFERENCES `event` (`event_pin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `guest`
+--
+
+LOCK TABLES `guest` WRITE;
+/*!40000 ALTER TABLE `guest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `planner`
+--
+
+DROP TABLE IF EXISTS `planner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `planner` (
+  `planner_id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `date_added` datetime NOT NULL,
+  PRIMARY KEY (`planner_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `planner`
+--
+
+LOCK TABLES `planner` WRITE;
+/*!40000 ALTER TABLE `planner` DISABLE KEYS */;
+/*!40000 ALTER TABLE `planner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `preferences`
+--
+
+DROP TABLE IF EXISTS `preferences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `preferences` (
+  `preferences_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  PRIMARY KEY (`preferences_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `preferences_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `preferences`
+--
+
+LOCK TABLES `preferences` WRITE;
+/*!40000 ALTER TABLE `preferences` DISABLE KEYS */;
+/*!40000 ALTER TABLE `preferences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `preferences_guest`
+--
+
+DROP TABLE IF EXISTS `preferences_guest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `preferences_guest` (
+  `preferences_guest_id` int(11) NOT NULL,
+  `preferences_id` int(11) NOT NULL,
+  `guest_id` int(11) NOT NULL,
+  KEY `guest_id` (`guest_id`),
+  KEY `preferences_id` (`preferences_id`),
+  CONSTRAINT `preferences_guest_ibfk_1` FOREIGN KEY (`guest_id`) REFERENCES `guest` (`guest_id`),
+  CONSTRAINT `preferences_guest_ibfk_2` FOREIGN KEY (`preferences_id`) REFERENCES `preferences` (`preferences_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `preferences_guest`
+--
+
+LOCK TABLES `preferences_guest` WRITE;
+/*!40000 ALTER TABLE `preferences_guest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `preferences_guest` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-19 16:12:08
