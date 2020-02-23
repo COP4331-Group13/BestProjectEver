1aed8aebd92ec37d00f78adb2ded04ec5cf9773c
Bruno Ebihara <49998058+brunoebihara@users.noreply.github.com>
2020-02-23 17:27:58 -0500

    Update config.js

--- seating-chart/src/server/config.js
+++ seating-chart/src/server/config.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host     : '35.243.169.229',
   user     : 'test',
   password : 'MdbTesting123!',
-  database : 'test'
+  database : 'seatplanner'
 });
 
 connection.connect(function(err){
