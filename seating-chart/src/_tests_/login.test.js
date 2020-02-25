8ee5e0154b5153c5f721295d365250d27cfec003
grizzlyThresher <grizzlyThresher@gmail.com>
2020-02-24 23:17:58 -0500

    Adds Semi-Functioning Event List without CSS

--- seating-chart/src/_tests_/login.test.js
+++ seating-chart/src/_tests_/login.test.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {cleanup} from '@testing-library/react';
-import {LoginBox, PlannerLogin, GuestLogin} from "../pages/Login";
-import shallow from "enzyme/shallow";
+import '../services/LocalStorage'
+import App from "../App";
+import {fireEvent} from "@testing-library/dom";
 
 afterEach(cleanup);
 
-test('renders LoginScreen without crashing', () => {
+test('Application renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<LoginBox />, div);
+    ReactDOM.render(<App />, div);
 });
 
-test('renders PlannerLogin without crashing', () => {
+test('App starts on Login Screen',() => {
     const div = document.createElement('div');
-    ReactDOM.render(<PlannerLogin />, div);
-});
-
-test('cannot move to Planner screen on null input',() => {
-    const wrapper = shallow(<PlannerLogin />);
-    global.window = { location: { pathname: '/' } };
-    const loginButton = wrapper.find('input').at(2);
-    loginButton.simulate('click');
+    ReactDOM.render(<App />, div);
+    global.window = { location: { pathname: '' } };
     expect(global.window.location.pathname).toEqual('/');
 
 });
 
-test('renders GuestLogin without crashing', () => {
+test('App Has Planner Login at startup',() => {
     const div = document.createElement('div');
-    ReactDOM.render(<GuestLogin />, div);
+    ReactDOM.render(<App />, div);
+    global.window = { location: { pathname: '' } };
+    expect(global.window.location.pathname).toEqual('/');
+    expect(div.getElementsByClassName('plannerLogin')[0]).toBeInTheDocument;
+
+});
+
+test('App Has Guest Login at startup',() => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    global.window = { location: { pathname: '' } };
+    expect(global.window.location.pathname).toEqual('/');
+    console.log(div.getElementsByClassName('box'));
+    expect(div.getElementsByClassName('guestLogin')[0]).toBeInTheDOM;
+
+});
+
+test('cannot move to Planner screen on null input',() => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    global.window = { location: { pathname: '/' } };
+    expect(global.window.location.pathname).toEqual('/');
+    const plannerLogin = div.getElementsByClassName('box');
+    console.log(div.getElementsByClassName('box'));
+    const login = plannerLogin.getElementsByClassName('button')[0];
+    fireEvent.click(login);
+    expect(global.window.location.pathname).toEqual('/');
+
 });
--- seating-chart/src/pages/Events/index.js
+++ seating-chart/src/pages/Events/index.js
@@ -6,16 +6,34 @@ export class EventList extends React.Component {
 
     constructor(props) {
         super(props);
-        const eventList = this.props.storage.getEvents();
+        this.state = {eventList:this.props.storage.getEvents(), listItems:[]};
+        for (let i = 0; i < this.state.eventList.length; i++) {
+            this.state.listItems.push(
+                <EventItem
+                    Key={i}
+                    Event={this.state.eventList[i] + i}
+                    storage = {this.props.storage}
+                    history = {this.props.history}
+                />);
+        }
+        this.props.storage.setEvent(undefined);
         this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
 
     handleSubmit(event) {
         event.preventDefault();
         this.props.storage.addEvent('Event');
-        this.props.storage.setEvent('Event');
-        this.props.history.push('/planner');
+        this.setState({eventList: this.props.storage.getEvents()});
+        this.setState(prevState => ({
+            listItems: [...prevState.listItems, <EventItem
+                Key={this.state.eventList.length - 1}
+                Event = {'Event' + (this.state.eventList.length)}
+                storage = {this.props.storage}
+                history = {this.props.history}
+            />]
+        }));
     }
 
     render() {
@@ -23,6 +41,9 @@ export class EventList extends React.Component {
             <div id="wrapperbox">
                 <div id = "welcome">
                     <h1>Welcome, {this.props.storage.getUser()}!</h1>
+                </div>
+                <div id="listWrapper">
+                    <ul id="eventList">{this.state.listItems}</ul>
                     <input type='submit' className='button' id='add_event' value='Add Event' onClick={this.handleSubmit}/>
                 </div>
             </div>
@@ -30,6 +51,24 @@ export class EventList extends React.Component {
     }
 }
 
+class EventItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.goToEvent = this.goToEvent.bind(this);
+    }
+
+    goToEvent(event) {
+        event.preventDefault();
+        this.props.storage.setEvent(this.props.Event);
+        this.props.history.push('/planner');
+    }
+
+    render() {
+        return (
+            <li key={this.props.Key} className='eventItem' onClick={this.goToEvent}>{this.props.Event}</li>
+        );
+    }
+}
 
 
 export default withRouter(EventList);
