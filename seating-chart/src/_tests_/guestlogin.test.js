57c19f6c7626268db159ff4b282a50ba5a9dfc70
hoytk6 <hoytk@knights.ucf.edu>
2020-03-29 11:38:42 -0400

    created login tests

--- seating-chart/src/_tests_/guestlogin.test.js
+++ seating-chart/src/_tests_/guestlogin.test.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom';
 import {cleanup} from '@testing-library/react';
 import '../services/LocalStorage';
 import App from '../App';
-import {fireEvent} from "@testing-library/dom';
+import {fireEvent} from '@testing-library/dom';
 
 afterEach(cleanup);
 
 // test correct guest code
+test('Validated guest credentials can log in', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        global.window = { location: { pathname: '/' } };
+        expect(global.window.location.pathname).toEqual('/');
+        const guestLogin = div.getElementsByClassName('box');
+        console.log(div.getElementsByClassName('box'));
+        // check for 200 response from server
+});
 
 // test incorrect guest code (wrong number of characters, code not in db, etc)
+test('Invalid guest credentials cannot log in', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        global.window = { location: { pathname: '/' } };
+        expect(global.window.location.pathname).toEqual('/');
+        const guestLogin = div.getElementsByClassName('box');
+        console.log(div.getElementsByClassName('box'));
+        // check for 205 response from server
+});
+
 
 // test blank guest code
+test('cannot move to Guest screen on null input',() => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    global.window = { location: { pathname: '/' } };
+    expect(global.window.location.pathname).toEqual('/');
+    const guestLogin = div.getElementsByClassName('box');
+    console.log(div.getElementsByClassName('box'));
+    const login = guestLogin.getElementsByClassName('button')[0];
+    fireEvent.click(login);
+    expect(global.window.location.pathname).toEqual('/');
+
+});
--- seating-chart/src/_tests_/plannerlogin.test.js
+++ seating-chart/src/_tests_/plannerlogin.test.js
@@ -8,6 +8,25 @@ import {fireEvent} from "@testing-library/dom";
 afterEach(cleanup);
 
 // test correct email/pw
+test('Validated planner credentials can log in', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	global.window = { location: { pathname: '/' } };
+	expect(global.window.location.pathname).toEqual('/');
+	const plannerLogin = div.getElementsByClassName('box');
+	console.log(div.getElementsByClassName('box'));
+	// check for 200 response from server
+});
 
 // test incorrect email/pw (misspelled, not in db, etc)
+test('Invalid planner credentials cannot log in', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        global.window = { location: { pathname: '/' } };
+        expect(global.window.location.pathname).toEqual('/');
+        const plannerLogin = div.getElementsByClassName('box');
+        console.log(div.getElementsByClassName('box'));
+        // check for 205 response from server
+});
+
 
